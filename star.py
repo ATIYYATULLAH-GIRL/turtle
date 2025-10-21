1076e62b5370786ad8d600c71de829f1f463b642
ATIYYATULLAH-GIRL <mabas2014.am@gmail.com>
2025-10-21 10:00:25 -0700

    Add files via upload

---
+++ star.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.Screen().bgcolor("aqua")
+turtle.Screen().setup(400,400)
+
+star=turtle.Turtle()
+length=100
+angle=144
+
+for i in range(5):
+    star.forward(length)
+    star.right(angle)
+turtle.close()
